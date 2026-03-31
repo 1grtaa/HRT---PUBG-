a454e20dd91390fe1a97c41ea60dacfd61a6bd6e
1grtaa <grtaa85@gmail.com>
2026-03-31 15:06:58 +0300

    Remove start and load functions
    Removed the start and load functions from app.js.

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-async function start() {
-    await fetch("/start", { method: "POST" });
-    setInterval(load, 3000);
-}
 
-async function load() {
-    let res = await fetch("/data");
-    let data = await res.json();
-
-    let div = document.getElementById("logs");
-    div.innerHTML = "";
-
-    data.logs.forEach(l => {
-        let p = document.createElement("p");
-        p.innerText = JSON.stringify(l);
-        div.appendChild(p);
-    });
-}
