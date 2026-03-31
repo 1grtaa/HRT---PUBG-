010f86f55cd71bb3a7949c94f68717154260debb
1grtaa <grtaa85@gmail.com>
2026-03-31 20:05:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
+const API = "https://YOUR-RAILWAY-LINK";
 
+async function addUser() {
+
+    await fetch(API + "/add-user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            api: document.getElementById("api").value,
+            secret: document.getElementById("secret").value,
+            amount: document.getElementById("amount").value,
+            targetProfit: document.getElementById("profit").value
+        })
+    });
+
+    alert("تم بدء التداول");
+}
+
+async function getSignal() {
+
+    let res = await fetch(API + "/signal");
+    let data = await res.json();
+
+    let el = document.createElement("p");
+    el.innerText = `${data.signal} | ${data.price}`;
+
+    document.getElementById("signals").prepend(el);
+}
+
+async function getUsers() {
+    let res = await fetch(API + "/users-count");
+    let data = await res.json();
+    document.getElementById("users").innerText = data.count;
+}
+
+setInterval(getSignal, 5000);
+setInterval(getUsers, 5000);
